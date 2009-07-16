3c34f9a2f88a58aed62506491303c702972dd291
Marco Fontani <mfontani@cpan.org>
2009-07-16 19:42:01 +0100

    Added script to list limits

---
+++ scripts/list_limits.pl
@@ -0,0 +1,17 @@
+#!perl
+use strict;
+use warnings;
+use Net::Mosso::CloudServers;
+
+my $user = $ENV{'CLOUDSERVERS_USER'} or die "Need CLOUDSERVERS_USER environment variable set";
+my $key  = $ENV{'CLOUDSERVERS_KEY'} or die "Need CLOUDSERVERS_KEY environment variable set";
+
+$Net::Mosso::CloudServers::DEBUG = 1;
+my $CS = Net::Mosso::CloudServers->new(
+  user => $user,
+  key  => $key,
+);
+
+$CS->limits;
+
+
