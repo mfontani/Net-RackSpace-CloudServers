99e44f8d1053ff3854ce45620a402b406be70a11
Marco Fontani <mfontani@cpan.org>
2009-11-18 23:44:02 +0000

    describing what is going on

--- scripts/list_servers.pl
+++ scripts/list_servers.pl
@@ -7,12 +7,13 @@ my $user = $ENV{'CLOUDSERVERS_USER'} or die "Need CLOUDSERVERS_USER environment 
 my $key  = $ENV{'CLOUDSERVERS_KEY'}  or die "Need CLOUDSERVERS_KEY environment variable set";
 
 $Net::RackSpace::CloudServers::DEBUG = 1;
+warn "** Creating Net::RackSpace::CloudServers object..\n";
 my $CS = Net::RackSpace::CloudServers->new(
   user => $user,
   key  => $key,
 );
-
+warn "** \$CS->get_server()..\n";
 $CS->get_server();
-$CS->get_server(1);
+warn "** \$CS->get_server_detail..\n";
 $CS->get_server_detail();
 
