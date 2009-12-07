0d9f904e209eabb63b199a2805771b1e34b8f5d6
Marco Fontani <mfontani@cpan.org>
2009-12-07 06:53:26 +0000

    Implemented --quiet option, should make the output possible to be piped

--- lib/App/CloudServers/Command/list.pm
+++ lib/App/CloudServers/Command/list.pm
@@ -18,6 +18,7 @@ sub opt_spec {
     [ 'ips|p',     'enables --details, and also list servers IP addresses info' ],
     [ 'limits|l',  'lists how many requests can still be done' ],
     [ 'table|t',   'uses Text::SimpleTable to draw the report' ],
+    [ 'quiet|q',   'only output data (useful for piping)' ],
     [ 'user',      'specify cloudservers API user, instead of $ENV{CLOUDSERVERS_USER}' ],
     [ 'key',       'specify cloudservers API key, instead of $ENV{CLOUDSERVERS_KEY}' ],
     [ 'help|h',    'get help for this command' ],
@@ -35,6 +36,7 @@ sub validate_args {
   $self->usage_error("--details cannot be used for --limits\n")
     if ( defined $opt->{details} && defined $opt->{limits} );
   $opt->{details} //= 0;
+  $opt->{quiet}   //= 0;
   $self->usage_error("--ips can be used for --servers only\n")
     if ( defined $opt->{ips} && !defined $opt->{servers} );
   $opt->{ips} //= 0;
@@ -46,6 +48,8 @@ sub validate_args {
   $opt->{user}  //= $ENV{CLOUDSERVERS_USER};
   $opt->{key}   //= $ENV{CLOUDSERVERS_KEY};
   $opt->{table} //= 0;
+  $self->usage_error("--quiet cannot be used with --table\n")
+    if ( $opt->{table} && $opt->{quiet} );
   $self->usage_error("No args allowed\n") if @$args;
 }
 
@@ -55,105 +59,97 @@ sub run {
     user => $opt->{user},
     key  => $opt->{key},
   );
-  _list_flavors( $CS, $opt->{details}, $opt->{table} ) if ( $opt->{flavors} );
-  _list_images( $CS, $opt->{details}, $opt->{table} ) if ( $opt->{images} );
-  _list_servers( $CS, $opt->{details}, $opt->{table}, $opt->{ips} ) if ( $opt->{servers} );
-  _list_limits( $CS, $opt->{table} ) if ( $opt->{limits} );
+  _list_flavors( $CS, $opt ) if ( $opt->{flavors} );
+  _list_images( $CS, $opt ) if ( $opt->{images} );
+  _list_servers( $CS, $opt ) if ( $opt->{servers} );
+  _list_limits( $CS, $opt ) if ( $opt->{limits} );
 }
 
 sub _list_flavors {
-  my ( $CS, $details, $use_table ) = @_;
-  my @flavors = $details ? $CS->get_flavor_detail() : $CS->get_flavor();
-  say "Listing flavors", $details ? ' details' : '';
+  my ( $CS, $opt ) = @_;
+  my @flavors = $opt->{details} ? $CS->get_flavor_detail() : $CS->get_flavor();
+  say "Listing flavors", $opt->{details} ? ' details' : '' unless $opt->{quiet};
   my $table;
   my $fmt;
-
-  if ($use_table) {
+  my $details = $opt->{details};
+  if ( $opt->{table} ) {
     use Text::SimpleTable;
     $table = Text::SimpleTable->new(
       [ 4,  'id' ],
       [ 14, 'name' ],
       (
         $details
-        ? (
-          [ 8, 'disk' ],
-          [ 8,  'ram' ],
-          )
+        ? ( [ 8, 'disk' ], [ 8, 'ram' ], )
         : ()
       )
     );
-  } else {
+  } elsif ( !$opt->{quiet} ) {
     $fmt = '  %-4s %-14s' . ( $details ? ' %-8s %-8s' : '' );
-    say
-      sprintf( $fmt, qw/id name/, $details ? qw/disk ram/ : undef );
-    say '  ----+--------------',
-      $details ? '+--------+--------+' : '';
+    say sprintf( $fmt, qw/id name/, $details ? qw/disk ram/ : undef );
+    say '  ----+--------------', $details ? '+--------+--------+' : '';
+  } else {    # quiet
+    $fmt = '%s "%s"' . ( $details ? ' %s %s' : '' );
   }
 
   foreach my $fla (@flavors) {
     my @det =
-      map { $fla->$_ // '' }
-      ( qw/id name/, ( $details ? (qw/disk ram/) : () ) );
-    if ($use_table) {
+      map { $fla->$_ // '' } ( qw/id name/, ( $details ? (qw/disk ram/) : () ) );
+    if ( $opt->{table} ) {
       $table->row(@det);
     } else {
       say sprintf( $fmt, @det );
     }
   }
-  say $table->draw if $use_table;
+  say $table->draw if $opt->{table};
 }
 
 sub _list_images {
-  my ( $CS, $details, $use_table ) = @_;
-  my @images = $details ? $CS->get_image_detail() : $CS->get_image();
-  say "Listing images", $details ? ' details' : '';
+  my ( $CS, $opt ) = @_;
+  my @images = $opt->{details} ? $CS->get_image_detail() : $CS->get_image();
+  say "Listing images", $opt->{details} ? ' details' : '' unless $opt->{quiet};
   my $table;
   my $fmt;
-
-  if ($use_table) {
+  my $details = $opt->{details};
+  if ( $opt->{table} ) {
     use Text::SimpleTable;
     $table = Text::SimpleTable->new(
       [ 6,  'id' ],
       [ 26, 'name' ],
       (
         $details
-        ? (
-          [ 7, 'status' ],
-          [ 25, 'created' ],
-          [ 25, 'updated' ],
-          )
+        ? ( [ 7, 'status' ], [ 25, 'created' ], [ 25, 'updated' ], )
         : ()
       )
     );
-  } else {
+  } elsif ( !$opt->{quiet} ) {
     $fmt = '  %-6s %-26s' . ( $details ? ' %-7s %-25s %-25s' : '' );
-    say
-      sprintf( $fmt, qw/id name/, $details ? qw/status created updated/ : undef );
+    say sprintf( $fmt, qw/id name/, $details ? qw/status created updated/ : undef );
     say '  ------+--------------------------',
       $details ? '+-------+-------------------------+-------------------------' : '';
+  } else {
+    $fmt = '%s "%s"' . ( $details ? ' %s %s %s' : '' );
   }
 
   foreach my $img (@images) {
     my @det =
-      map { $img->$_ // '' }
-      ( qw/id name/, ( $details ? (qw/status created updated/) : () ) );
-    if ($use_table) {
+      map { $img->$_ // '' } ( qw/id name/, ( $details ? (qw/status created updated/) : () ) );
+    if ( $opt->{table} ) {
       $table->row(@det);
     } else {
       say sprintf( $fmt, @det );
     }
   }
-  say $table->draw if $use_table;
+  say $table->draw if $opt->{table};
 }
 
 sub _list_servers {
-  my ( $CS, $details, $use_table, $show_ips ) = @_;
-  my @servers = $details ? $CS->get_server_detail() : $CS->get_server();
-  say "Listing servers", $details ? ' details' : '', $show_ips ? ' and IP addresses' : '';
+  my ( $CS, $opt ) = @_;
+  my @servers = $opt->{details} ? $CS->get_server_detail() : $CS->get_server();
+  say "Listing servers", $opt->{details} ? ' details' : '', $opt->{ips} ? ' and IP addresses' : '';
   my $table;
   my $fmt;
-
-  if ($use_table) {
+  my $details = $opt->{details};
+  if ( $opt->{table} ) {
     use Text::SimpleTable;
     $table = Text::SimpleTable->new(
       [ 8,  'id' ],
@@ -161,67 +157,95 @@ sub _list_servers {
       (
         $details
         ? (
-          [ 32, $show_ips ? 'hostid and IPs' : 'hostid' ],
-          [ 18,  'flavor' ],
-          [ 28,  'image' ],
+          [ 32, $opt->{ips} ? 'hostid and IPs' : 'hostid' ],
+          [ 18, 'flavor' ],
+          [ 28, 'image' ],
           [ 8,  'progress' ],
           [ 10, 'status' ]
           )
         : ()
       )
     );
-  } else {
+  } elsif ( !$opt->{quiet} ) {
     $fmt = '  %-8s %-12s' . ( $details ? ' %-32s %-18s %-28s %-8s %s' : '' );
-    say
-      sprintf( $fmt, qw/id name/, $details ? ($show_ips?'hostid and IPs':'hostid', qw/flavor image progress status/) : undef );
+    say sprintf( $fmt,
+      qw/id name/,
+      $details
+      ? ( $opt->{ips} ? 'hostid and IPs' : 'hostid', qw/flavor image progress status/ )
+      : undef );
     say '  --------+------------',
-      $details ? '+--------------------------------+------------------'
-      . '+----------------------------+--------+--------------' : '';
+      $details
+      ? '+--------------------------------+------------------'
+      . '+----------------------------+--------+--------------'
+      : '';
+  } else {
+    $fmt = '%s %s' . ( $details ? ' %s %s %s %s %s' : '' );
   }
 
   foreach my $srv (@servers) {
     my $flavor;
-    $flavor = $srv->flavorid . ' ' . $CS->get_flavor($srv->flavorid)->name if ($details);
     my $image;
-    $image = $srv->imageid . ' ' . $CS->get_image($srv->imageid)->name if ($details);
+    if ($details) {
+      $flavor = $srv->flavorid . ' ';
+      $flavor .= '"' if ( $opt->{quiet} );
+      $flavor .= $CS->get_flavor( $srv->flavorid )->name;
+      $flavor .= '"' if ( $opt->{quiet} );
+      $image = $srv->imageid . ' ';
+      $image .= '"' if ( $opt->{quiet} );
+      $image .= $CS->get_image( $srv->imageid )->name;
+      $image .= '"' if ( $opt->{quiet} );
+    }
     my $hostip;
-    if ($show_ips) {
-      $hostip = [
-        'Public:  ' . join(' ', map { $_ } @{$srv->public_address}),
-        'Private: ' . join(' ', map { $_ } @{$srv->private_address})
-      ];
+    if ( $opt->{ips} ) {
+      if ( $opt->{quiet} ) {
+        $hostip = [
+          'Public ' . join( ' ', map { $_ } @{ $srv->public_address } ),
+          'Private ' . join( ' ', map { $_ } @{ $srv->private_address } )
+        ];
+      } else {
+        $hostip = [
+          'Public:  ' . join( ' ', map { $_ } @{ $srv->public_address } ),
+          'Private: ' . join( ' ', map { $_ } @{ $srv->private_address } )
+        ];
+      }
     }
     my @det = (
-      ( map { $srv->$_ // '' } ( qw/id name/ ) ),
-      $details ? (
-        $use_table
-        ? $srv->hostid . "\n" . join("\n",@$hostip)
+      ( map { $srv->$_ // '' } (qw/id name/) ),
+      $details
+      ? (
+          $opt->{table}
+        ? $srv->hostid . "\n" . join( "\n", @$hostip )
         : $srv->hostid,
         $flavor, $image, $srv->progress, $srv->status
-      ) : ()
+        )
+      : ()
     );
-    if ($use_table) {
+    if ( $opt->{table} ) {
       $table->row(@det);
     } else {
       say sprintf( $fmt, @det );
-      if ($show_ips) {
+      if ( $opt->{ips} ) {
         foreach (@$hostip) {
-          say sprintf($fmt,'','',$_,'','','','','');
+          if ( $opt->{quiet} ) {
+            say sprintf( '%s "%s"', $srv->id, $_ );
+          } else {
+            say sprintf( $fmt, '', '', $_, '', '', '', '', '' );
+          }
         }
       }
     }
   }
-  say $table->draw if $use_table;
+  say $table->draw if $opt->{table};
 }
 
 sub _list_limits {
-  my ( $CS, $use_table ) = @_;
+  my ( $CS, $opt ) = @_;
   my $cs = $CS->limits;
   if ( defined $cs->rate && ref $cs->rate eq 'ARRAY' ) {
-    say "Rate limits:";
+    say "Rate limits:" unless $opt->{quiet};
     my $table;
     my $fmt;
-    if ($use_table) {
+    if ( $opt->{table} ) {
       use Text::SimpleTable;
       $table = Text::SimpleTable->new(
         [ 6,  'verb' ],
@@ -232,11 +256,13 @@ sub _list_limits {
         [ 7,  'progress' ],
         [ 35, 'reset-time (local time)' ]
       );
-    } else {
+    } elsif ( !$opt->{quiet} ) {
       $fmt = '  %-8s %-16s %-16s %-6s %-6s %-6s (%s) %s';
       say sprintf( $fmt, qw/verb URI regex value left units reset-time local-time/ );
       say '  --------+----------------+----------------+------+------+',
         '------+------------+------------------------';
+    } else {
+      $fmt = '%s "%s" "%s" %s %s %s %s "%s"';
     }
     foreach my $rl ( @{ $cs->rate } ) {
       my @reset_time = (
@@ -250,21 +276,25 @@ sub _list_limits {
         $rl->{value}     // 'n/a',
         $rl->{remaining} // 'n/a',
         $rl->{unit}      // 'n/a',
-        $use_table ? join( ' ', @reset_time ) : @reset_time
+        $opt->{table} ? join( ' ', @reset_time ) : @reset_time
       );
-      if ($use_table) {
+      if ( $opt->{table} ) {
         $table->row(@det);
       } else {
         say sprintf( $fmt, @det );
       }
     }
-    say $table->draw if $use_table;
+    say $table->draw if $opt->{table};
   } else {
-    say "No rate info found or not an array: ", ref $cs->rate;
+    warn "No rate info found or not an array: ", ref $cs->rate;
   }
-  say "Absolute limits:";
+  say "Absolute limits:" unless $opt->{quiet};
   foreach my $k (qw/totalramsize maxipgroups maxipgroupmembers/) {
-    say sprintf( '  %-20s %s', $k, $cs->$k // 'n/a' );
+    if ( $opt->{quiet} ) {
+      say sprintf( '%s %s', $k, $cs->$k // 'n/a' );
+    } else {
+      say sprintf( '  %-20s %s', $k, $cs->$k // 'n/a' );
+    }
   }
 }
 
