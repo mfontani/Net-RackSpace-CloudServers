82f7bf2ad9b7546aedbfacba98da2058b6f050e0
Marco Fontani <mfontani@cpan.org>
2009-12-08 08:27:45 +0000

    Implemented checks for images and flavors; passing over object to run() method via options

--- lib/App/CloudServers/Command/create.pm
+++ lib/App/CloudServers/Command/create.pm
@@ -22,9 +22,10 @@ sub opt_spec {
     [ 'metadata|m=s%', 'specify max 5 255B string metadata keys/values', { default => {} } ],
     [ 'path|p=s%',   'specify max 5 10kB max files to be pushed to the server', { default => {} } ],
     [ 'groupid|g=s', 'specify shared ip group ID for server',                   { default => '' } ],
-    [ 'user',   'specify cloudservers API user, instead of $ENV{CLOUDSERVERS_USER}' ],
-    [ 'key',    'specify cloudservers API key, instead of $ENV{CLOUDSERVERS_KEY}' ],
-    [ 'help|h', 'get help for this command' ],
+    [ 'user', 'specify cloudservers API user, instead of $ENV{CLOUDSERVERS_USER}' ],
+    [ 'key',  'specify cloudservers API key, instead of $ENV{CLOUDSERVERS_KEY}' ],
+    [ 'verbose', 'show more information', { default => 0 } ],
+    [ 'help|h',  'get help for this command' ],
   );
 }
 
@@ -32,19 +33,27 @@ sub validate_args {
   my ( $self, $opt, $args ) = @_;
   die $self->_usage_text if $opt->{help};
   use YAML;
-  warn "Options specified: ", Dump($opt);
+  warn '*' x 72, "\n", "Options specified: ", Dump($opt), '*' x 72, "\n";
   $self->usage_error("use --user or defined \$ENV{CLOUDSERVERS_USER} to use this command\n")
     if ( !defined $opt->{user} && !defined $ENV{CLOUDSERVERS_USER} );
   $self->usage_error("use --key or defined \$ENV{CLOUDSERVERS_KEY} to use this command\n")
     if ( !defined $opt->{key} && !defined $ENV{CLOUDSERVERS_KEY} );
+  $self->usage_error("No args allowed\n") if @$args;
   if ( !defined $opt->{name} || !length $opt->{name} ) {
     $self->usage_error("Specify a --name for the new server\n");
   }
+  say "Server name: ", $opt->{name} if ( $opt->{verbose} );
   if ( defined $opt->{imageid} && defined $opt->{imagename} ) {
     $self->usage_error("Specify either --imageid or --imagename, not both\n");
   }
+  unless ( defined $opt->{imageid} || defined $opt->{imagename} ) {
+    $self->usage_error("Specify either --imageid or --imagename\n");
+  }
   if ( defined $opt->{flavorid} && defined $opt->{flavorname} ) {
     $self->usage_error("Specify either --flavorid or --flavorname, not both\n");
+  }
+  unless ( defined $opt->{flavorid} || defined $opt->{flavorname} ) {
+    $self->usage_error("Specify either --flavorid or --flavorname\n");
   }
   if ( keys %{ $opt->{metadata} } > 5 ) {
     $self->usage_error("You can only specify a maximum of 5 --metadata key/value pairs\n");
@@ -55,6 +64,8 @@ sub validate_args {
     $self->usage_error("$k: metadata values can only be 255B maximum in length\n")
       if ( length $opt->{metadata}->{$k} > 255 );
   }
+  say "Metadata: ", join( "\n", map { '  - ' . $_ } keys %{ $opt->{metadata} } )
+    if ( $opt->{verbose} );
   if ( keys %{ $opt->{path} } > 5 ) {
     $self->usage_error("You can only specify a maximum of 5 --path key/filename pairs\n");
   }
@@ -66,23 +77,62 @@ sub validate_args {
     $self->usage_error("$f: file size of file $opt->{path}->{$f} > 10kB\n")
       unless ( -s $opt->{path}->{$f} <= 10_000 );
   }
-  die "NOT IMPLEMENTED\n";
-
-  # TODO: groupid
-  # TODO: check image id or name exist
-  # TODO: check flavor id or name exist
-  $opt->{user}  //= $ENV{CLOUDSERVERS_USER};
-  $opt->{key}   //= $ENV{CLOUDSERVERS_KEY};
-  $opt->{table} //= 0;
-  $self->usage_error("No args allowed\n") if @$args;
-}
-
-sub run {
-  my ( $self, $opt, $args ) = @_;
+  say "Paths: ", join( "\n", map { '  - ' . $_ } keys %{ $opt->{path} } ) if ( $opt->{verbose} );
+  $opt->{user} //= $ENV{CLOUDSERVERS_USER};
+  $opt->{key}  //= $ENV{CLOUDSERVERS_KEY};
   my $CS = Net::RackSpace::CloudServers->new(
     user => $opt->{user},
     key  => $opt->{key},
   );
+  my @images = $CS->get_image();
+  if ( !defined $opt->{imageid} ) {    # defined imagename
+    my @_img = grep { $_->name =~ /\Q$opt->{imagename}\E/i } @images;
+    $self->usage_error("image name $opt->{imagename} not found in image list\n") unless (@_img);
+    unless ( @_img == 1 ) {
+      $self->usage_error( "Too many matches for image name $opt->{imagename}:\n"
+          . join( "\n", map { '  - ' . $_->name } @_img ) );
+    }
+    $opt->{imageid}   = $_img[0]->id;
+    $opt->{imagename} = $_img[0]->name;
+  }
+  {
+    my @_img = grep { $_->id eq $opt->{imageid} } @images;
+    $self->usage_error("image id $opt->{imageid} not found in image list\n") unless (@_img);
+    $self->usage_error("Too many matches for image id $opt->{imageid}\n") unless ( @_img == 1 );
+    $opt->{imagename} = $_img[0]->name;
+  }
+  say "Image id ", $opt->{imageid}, " named ", $opt->{imagename} if ( $opt->{verbose} );
+  my @flavors = $CS->get_flavor();
+  if ( !defined $opt->{flavorid} ) {
+    my @_fla = grep { $_->name =~ /\Q$opt->{flavorname}\E/i } @flavors;
+    $self->usage_error("image name $opt->{flavorname} not found in flavorlist\n") unless (@_fla);
+    unless ( @_fla == 1 ) {
+      $self->usage_error( "Too many matches for flavor name $opt->{flavorname}:\n"
+          . join( "\n", map { '  - ' . $_->name } @_fla ) );
+    }
+    $opt->{flavorid}   = $_fla[0]->id;
+    $opt->{flavorname} = $_fla[0]->name;
+  }
+  {
+    my @_fla = grep { $_->id eq $opt->{flavorid} } @flavors;
+    $self->usage_error("flavor id $opt->{flavorid} not found in flavor list\n") unless (@_fla);
+    $self->usage_error("Too many matches for flavor id $opt->{flavorid}\n") unless ( @_fla == 1 );
+    $opt->{flavorname} = $_fla[0]->name;
+  }
+  say "Flavor id ", $opt->{flavorid}, " named ", $opt->{flavorname} if ( $opt->{verbose} );
+
+  # TODO: groupid
+  $opt->{__RACKSPACE_CLOUDSERVERS}         = $CS;
+  $opt->{__RACKSPACE_CLOUDSERVERS_IMAGES}  = \@images;
+  $opt->{__RACKSPACE_CLOUDSERVERS_FLAVORS} = \@flavors;
+}
+
+sub run {
+  my ( $self, $opt, $args ) = @_;
+  my $CS      = $opt->{__RACKSPACE_CLOUDSERVERS};
+  my @images  = @{ $opt->{__RACKSPACE_CLOUDSERVERS_IMAGES} };
+  my @flavors = @{ $opt->{__RACKSPACE_CLOUDSERVERS_FLAVORS} };
+  say "NOT IMPLEMENTED";
 }
 
 1;
